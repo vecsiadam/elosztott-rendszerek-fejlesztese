bde83fb0e90fd1fb06489d1818fac569273b58c3
adamvecsi <vecsi1994@hotmail.com>
2020-09-13 12:09:26 +0200

    fix: path variable

--- distributed-systems/src/main/java/com/example/distributedsystems/controller/EmployeeController.java
+++ distributed-systems/src/main/java/com/example/distributedsystems/controller/EmployeeController.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.distributedsystems.model.dto.EmployeeDTO;
@@ -44,7 +44,7 @@ public class EmployeeController {
 	}
 
 	@DeleteMapping("/delete-employee/{id}")
-	public void deleteEmployee(@RequestParam Long id) {
+	public void deleteEmployee(@PathVariable Long id) {
 		employeeService.deleteEmployee(id);
 	}
 }
